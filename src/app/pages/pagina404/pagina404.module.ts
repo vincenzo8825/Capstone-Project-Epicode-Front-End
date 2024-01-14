4d4bde9456b6c4b15c8fa30542cff6374aea37f2
vincenzo8825 <vincenzorocca88@gmail.com>
2024-01-14 23:23:43 +0100

    fix pagina 404

--- src/app/pages/pagina404/pagina404.module.ts
+++ src/app/pages/pagina404/pagina404.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'; // Aggiungi questa riga
 import { Pagina404Component } from './pagina404.component';
-
-
 
 @NgModule({
   declarations: [
     Pagina404Component
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule // Aggiungi RouterModule qui
   ]
 })
 export class Pagina404Module { }
